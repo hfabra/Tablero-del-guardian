99b6ddbe3f98777a7b0384883e88357eb4ad81cd
hfabra <hfabra@gmail.com>
2025-09-25 23:50:24 -0500

    Update retos.php

--- retos.php
+++ retos.php
@@ -290,8 +290,8 @@ $retos = $res->get_result();
     convert_urls: false,
     image_caption: true,
     image_title: true,
-    extended_valid_elements: 'iframe[src|frameborder|style|scrolling|class|width|height|name|title|allow|allowfullscreen|referrerpolicy|loading]',
-    content_style: "body { font-family: 'Inter', sans-serif; font-size: 16px; } iframe { max-width: 100%; border: none; }"
+    extended_valid_elements: 'iframe[src|width|height|frameborder|scrolling]',
+    content_style: "body { font-family: 'Inter', sans-serif; font-size: 16px; }"
   });
 </script>
 
